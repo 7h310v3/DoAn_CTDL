3900e50f27b9aa8008d4cf5d9d6bfbd7d6fddba0
7h310v3 <minh92dd@gmail.com>
2022-06-17 04:14:18 +0700

    thelove

--- BT05_tamquanhau.py
+++ BT05_tamquanhau.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-# Python program to solve N Queen
-# Problem using backtracking
 
 global N
 N = 8
@@ -13,25 +11,19 @@ def printSolution(board):
 		print()
 
 
-# A utility function to check if a queen can
-# be placed on board[row][col]. Note that this
-# function is called when "col" queens are
-# already placed in columns from 0 to col -1.
-# So we need to check only left side for
-# attacking queens
 def isSafe(board, row, col):
 
-	# Check this row on left side
+	# Kiểm tra hàng này ở phía bên trái
 	for i in range(col):
 		if board[row][i] == 1:
 			return False
 
-	# Check upper diagonal on left side
+	# Kiểm tra đường chéo trên ở phía bên trái
 	for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
 		if board[i][j] == 1:
 			return False
 
-	# Check lower diagonal on left side
+	# Kiểm tra đường chéo dưới bên trái
 	for i, j in zip(range(row, N, 1), range(col, -1, -1)):
 		if board[i][j] == 1:
 			return False
@@ -39,40 +31,30 @@ def isSafe(board, row, col):
 	return True
 
 def solveNQUtil(board, col):
-	# base case: If all queens are placed
-	# then return true
+	# Nếu tất cả các quân hậu được đặt thì trả về true
 	if col >= N:
 		return True
 
-	# Consider this column and try placing
-	# this queen in all rows one by one
+	# Xem xét cột này và thử đặt quân hậu này vào tất cả các hàng từng hàng một
 	for i in range(N):
 
 		if isSafe(board, i, col):
-			# Place this queen in board[i][col]
+			# Nếu đặt quân hậu tại vị trí [i][col] khả dụng thì thì vị trí [i][col] = 1
 			board[i][col] = 1
 
-			# recur to place rest of the queens
+			# Thử đặt tiếp các con hậu còn lại
 			if solveNQUtil(board, col + 1) == True:
 				return True
 
-			# If placing queen in board[i][col
-			# doesn't lead to a solution, then
-			# queen from board[i][col]
+			# Nếu đặt quân hậu tại vị trí [i][col] không được thì thì vị trí [i][col] = 0
 			board[i][col] = 0
 
-	# if the queen can not be placed in any row in
-	# this column col then return false
+	# nếu quân hậu không thể được đặt ở bất kỳ hàng nào trong cột này thì trả về false
 	return False
 
-# This function solves the N Queen problem using
-# Backtracking. It mainly uses solveNQUtil() to
-# solve the problem. It returns false if queens
-# cannot be placed, otherwise return true and
-# placement of queens in the form of 1s.
-# note that there may be more than one
-# solutions, this function prints one of the
-# feasible solutions.
+# Nó chủ yếu sử dụng giải quyếtNQUtil () để giải quyết vấn đề.
+# Nó trả về false nếu không thể đặt các quân hậu, ngược lại trả về true và vị trí các quân hậu ở dạng 1.
+# lưu ý rằng có thể có nhiều hơn một giải pháp, hàm này in ra một trong những giải pháp khả thi.
 def solveNQ():
 	board = np.zeros((N, N), int)
 
@@ -83,7 +65,8 @@ def solveNQ():
 	printSolution(board)
 	return True
 
-# driver program to test above function
-solveNQ()
+def main():
+	solveNQ()
 
-# This code is contributed by Divyanshu Mehta
+if __name__ == '__main__':
+	main()
