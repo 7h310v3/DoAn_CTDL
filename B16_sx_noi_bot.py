693d2b6d8cab45e4587384fb1ae43ff16cb446cb
7h310v3 <minh92dd@gmail.com>
2022-06-17 03:40:47 +0700

    thelove

--- B16_sx_noi_bot.py
+++ B16_sx_noi_bot.py
@@ -1,16 +1,34 @@
-#Sắp xếp theo thứ tự giảm dần
-x = int(input('Bạn muốn nhập bao nhiêu số: '))
-A = list()
-for i in range(0,x):
-    A.append(int(input('Nhập số thứ %d: ' % (i+1))))
-for i in range(x-1):
-     for j in range(i+1, x):
-         if A[i] > A[j]:
-             tam = A[j]
-             A[j] = A[i]
-             A[i] = tam 
+def bubbleSort(arr):
+    n = len(arr)
+    # flag
+    swapped = False
+    # Duyệt qua tất cả các phần tử mảng
+    for i in range(n - 1):
+        # Duyệt qua mảng từ 0 đến n-i-1
+        for j in range(0, n - i - 1):
+            # Hoán đổi nếu phần tử tìm được lớn hơn phần tử tiếp theo
+            if arr[j] > arr[j + 1]:
+                # flag
+                swapped = True
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
-# Cách khác:
-# A.sort()
+        if not swapped:
+            # nếu chúng ta không cần thực hiện một lần hoán đổi,
+            # chúng ta có thể thoát khỏi vòng lặp chính.
+            break
 
-print("====> Kết quả:", A)
+
+def main():
+    arr_t = [64, 34, 25, 12, 22, 11, 90]
+
+    bubbleSort(arr_t)
+
+    print("Mảng đã sắp xếp:", end="")
+    for i in range(len(arr_t)):
+        print("% d" % arr_t[i], end="\t")
+
+
+if __name__ == "__main__":
+    main()
+
+
